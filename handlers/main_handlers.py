2638b24f4aae763d183725ad6576a5e07cdd1f71
Suetosha <honey.pancake2017@yandex.ru>
2024-02-09 22:50:49 +0300

    testing

--- handlers/main_handlers.py
+++ handlers/main_handlers.py
@@ -1,3 +1,4 @@
+import requests
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart, StateFilter
@@ -12,21 +13,23 @@ router = Router()
 
 @router.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
-    print('1')
     user_id = message.from_user.id
-    print('2', user_id)
     data = get_from_db(queries.get_from_users(user_id))
-    print('3', data)
 
     if not data:
         add_to_db(queries.add_user(user_id))
 
-    print('4')
     await message.answer(LEXICON['start'])
 
 
 @router.message(Command(commands='help'), StateFilter(default_state))
 async def process_help_command(message: Message):
     await message.answer(LEXICON['help'])
+
+
+@router.message(Command(commands='get'), StateFilter(default_state))
+async def process_help_command(message: Message):
+    res = requests.get('https://animego.org')
+    await message.answer(str(res.status_code))
 
 
