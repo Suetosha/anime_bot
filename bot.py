e6e26e5fd1ae09676741a57eefb8d59b168eb54a
Suetosha <honey.pancake2017@yandex.ru>
2024-02-09 20:15:22 +0300

    Moved bot to webhook

--- bot.py
+++ bot.py
@@ -9,37 +9,38 @@ from parser import send_updates, get_new_anime, clean_up_table
 from datetime import datetime, date
 
 
-async def main() -> None:
-    config: Config = load_config()
+# async def main() -> None:
+config: Config = load_config()
 
-    storage = MemoryStorage()
-    bot = Bot(token=config.tg_bot.token)
-    dp = Dispatcher(storage=storage)
-    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-    # планировщик по отправке апдейтов
-    scheduler.add_job(send_updates, trigger='cron', hour=12, minute=00, kwargs={'bot': bot})
-    scheduler.add_job(send_updates, trigger='cron', hour=18, minute=00, kwargs={'bot': bot})
-    scheduler.add_job(send_updates, trigger='cron', hour=23, minute=59, kwargs={'bot': bot})
-    # планировщик по очищению таблицы рассылок
-    scheduler.add_job(clean_up_table, trigger='cron', hour=0, minute=0)
-    # планировщик по добавлениям аниме по сезонам
-    scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 1, 1), args=[1])
-    scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 4, 1), args=[4])
-    scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 7, 1), args=[7])
-    scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 10, 1), args=[10])
-    scheduler.start()
+bot = Bot(token=config.tg_bot.token)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
 
-    await set_main_menu(bot)
+scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+# планировщик по отправке апдейтов
+scheduler.add_job(send_updates, trigger='cron', hour=12, minute=00, kwargs={'bot': bot})
+scheduler.add_job(send_updates, trigger='cron', hour=18, minute=00, kwargs={'bot': bot})
+scheduler.add_job(send_updates, trigger='cron', hour=23, minute=59, kwargs={'bot': bot})
+# планировщик по очищению таблицы рассылок
+scheduler.add_job(clean_up_table, trigger='cron', hour=0, minute=0)
+# планировщик по добавлениям аниме по сезонам
+scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 1, 1), args=[1])
+scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 4, 1), args=[4])
+scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 7, 1), args=[7])
+scheduler.add_job(get_new_anime, trigger='date', run_date=date(datetime.now().year, 10, 1), args=[10])
+scheduler.start()
 
-    dp.include_router(main_handlers.router)
-    dp.include_router(commands_handlers.router)
+set_main_menu(bot)
 
-    dp.startup.register(set_main_menu)
+dp.include_router(main_handlers.router)
+dp.include_router(commands_handlers.router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+dp.startup.register(set_main_menu)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# bot.delete_webhook(drop_pending_updates=True)
+# dp.start_polling(bot)
+
+# if __name__ == '__main__':
+#     asyncio.run(main())
 
 
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from bot import bot, dp, config
+from aiogram import types
+
+
+WEBHOOK_PATH = f'/{config.tg_bot.token}/'
+WEBHOOK_URL = 'https://5919-87-116-163-213.ngrok-free.app' + WEBHOOK_PATH
+
+app = FastAPI()
+
+
+@app.on_event('startup')
+async def on_startup():
+    tg_bot_webhook = await bot.get_webhook_info()
+
+    if tg_bot_webhook != WEBHOOK_URL:
+        await bot.set_webhook(url=WEBHOOK_URL)
+
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict):
+    tg_update = types.Update(**update)
+    await dp._process_update(bot=bot, update=tg_update)
+    return 200
+
+
+@app.on_event('shutdown')
+async def on_shutdown():
+    pass
