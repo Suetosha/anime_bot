03bda22ce0ac71fcfa573a81655d36c1e73cabde
Suetosha <honey.pancake2017@yandex.ru>
2024-02-09 23:46:06 +0300

    testing

--- main.py
+++ main.py
@@ -13,36 +13,43 @@ WEBHOOK_PATH = f'/{env("BOT_TOKEN")}/'
 WEBHOOK_URL = 'https://a21e-87-116-163-213.ngrok-free.app' + WEBHOOK_PATH
 
 app = FastAPI()
-bot = Bot(token=env('BOT_TOKEN'))
-dp = Dispatcher()
+# bot = Bot(token=env('BOT_TOKEN'))
+# dp = Dispatcher()
 
 
 @app.on_event('startup')
 async def on_startup():
-    tg_bot_webhook = await bot.get_webhook_info()
-
-    if tg_bot_webhook != WEBHOOK_URL:
-        await bot.set_webhook(url=WEBHOOK_URL)
-
-
-@dp.message(Command('get'))
-async def get(message):
-    res = requests.get('https://google.com')
-    await message.answer(str(res.status_code))
+    pass
+    # tg_bot_webhook = await bot.get_webhook_info()
+    #
+    # if tg_bot_webhook != WEBHOOK_URL:
+    #     await bot.set_webhook(url=WEBHOOK_URL)
 
 
-@dp.message()
-async def wtf(message):
-    await message.answer('not found')
+# @dp.message(Command('get'))
+# async def get(message):
+#     res = requests.get('https://google.com')
+#     await message.answer(str(res.status_code))
+#
+#
+# @dp.message()
+# async def wtf(message):
+#     await message.answer('not found')
 
 
 @app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
     tg_update = types.Update(**update)
-    await dp.feed_update(bot=bot, update=tg_update)
+    # await dp.feed_update(bot=bot, update=tg_update)
     return 200
+
+
+@app.get('/')
+async def unhandled():
+    return 'huinya'
 
 
 @app.on_event('shutdown')
 async def on_shutdown():
-    await bot.session.close()
+    # await bot.session.close()
+    pass
